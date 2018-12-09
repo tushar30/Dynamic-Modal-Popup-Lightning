5a393b97a631769655c913572abfafc8308aa47a
Tushar Sharma <tusharsharma783@gmail.com>
2018-12-09 15:21:36 +0530

    Create LightningModelFooterCompController.JS

---
+++ LightningModelFooterCompController.JS
@@ -0,0 +1,9 @@
+({
+    handleCancel : function(component, event, helper) {
+        //closes the modal or popover from the component
+        component.find("overlayLib").notifyClose();
+    },
+    handleOK : function(component, event, helper) {
+        //do something
+    }
+})
